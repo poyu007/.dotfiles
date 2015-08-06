fae4e4910751981747bebcb40aa985f28da8aa6f
josh <josh@heisoo.com>
2015-08-06 22:13:56 +0800

    commit

--- script/dBee.php
+++ script/dBee.php
@@ -1,5 +1,4 @@
 <?php
- 
 include 'inc/basic.inc';
 include 'inc/shell_def.inc';
 $conf = is_file(__DIR__.'/inc/hs_conf.inc') ? __DIR__.'/inc/hs_conf.inc' : __DIR__.'/inc/conf.inc';
@@ -9,20 +8,35 @@ include($conf);
 //
 //a
 if(!isset($argv[1])){
-    e('miss parameter type -set , -dir ,-run, -do, -file, -add, -bind','red');
+    e('miss parameter type -set , -all , -file, -run , -do , -add, -bind','red');
 }else if(file_exists(UC_LOC) == false or $argv[1] == '-set') {
     init();
     exit;
+}else if(!isset($argv[2])){
+    e('miss 2 parameter ','red');
+}
+
+$source = realpath($argv[2]);
+if($source == ''){
+    e('argv 2 file not exist','red');
 }
 
 $_cf = loadConf(UC_LOC);
+$now_dir= (isset($argv[2])) ? $source : getenv('PWD');
+
+define_proj_dir($_cf['git_dir'],$_cf['user'],$now_dir);
+
+
 //loading config
 conf_to_def($_cf);
-$_pcf=loadConf(PCONF_LOC);
+if(is_file(PCONF_LOC)){
+    $_pcf=loadConf(PCONF_LOC);
+}else{
+    proj_setting($_cf);
+}
 
 check_version();
-
-
+// asdfsadfsfasdfasd 
 $arg = '';
 $num = count($argv);
 for($i=3; $i< $num ; $i++){
@@ -30,7 +44,8 @@ for($i=3; $i< $num ; $i++){
 }
 
 
-if($argv[1] == '-dir'){
+if($argv[1] == '-all'){
+
     proj_all();
 
 }else if($argv[1] == '-run'){
@@ -48,7 +63,7 @@ if($argv[1] == '-dir'){
     remote_do($argv[2]);
 }else if($argv[1] == '-file'){
 
-    $file = is_file($argv[2]) ? $argv[2] : e('file not exist',true);
+    $file = is_file($argv[2]) ? $argv[2] : e('file not exist','red');
     proj_file($file);
 
 }else if($argv[1] == '-add'){
